aeaaf4fdc68798a8d7dc362e16059b30d2ed9d66
RB Metcalf <bmetcalf@gmail.com>
2017-01-25 13:36:44 +0100

    Changed to use LightCones::FastLightCones() with a simulation box.

--- cones.cpp
+++ cones.cpp
@@ -29,62 +29,81 @@ static std::mutex barrier;
 
 int main(int arg,char **argv){
   
-  COSMOLOGY cosmo(Planck1yr);
-  Point_3d xo,v;
+  // output directory
+  const std::string outdir = "Output/";
   
-  std::vector<std::string> filenames;
-  std::vector<double> redshifts(1,0.0);
+  const COSMOLOGY cosmo(BigMultiDark);
+
+  //// stuff about observers and fields
+  const int Ncones = 10;
+  const double range = 6*degreesTOradians;
+  const double angular_resolution = range/512;
+  std::vector<double> zsources = {2.297,1.075,0.4892};
+  std::vector<std::vector<PixelMap> > maps;
+
+
+  /// stuff about the input snapshots
+  const double BoxLength = 2.5e3/cosmo.gethubble();
+  const float particle_mass = 2.359e10/cosmo.gethubble()/0.005;   // 0.5% of particles are  used
+  std::vector<std::string> snap_filenames;
+  std::vector<float> snap_redshifts;
+  
+  
   // random observer in the box
-  double boxwidth = 3.688948e3,hubble = 0.677700;
 
-  //std::string dir = "/data1/users/gustavo/BigMD/2.5_3840_Planck1/ROCKSTAR/out_";
-  //std::string suffix = "p.list";
-  std::string dir = "/data1/users/gustavo/BigMD/2.5_3840_Planck1/DM_SAMPLES/dm_particles_snap_0";
-  std::string suffix = ".dat";
-  
-  std::vector<std::string> num = {"77","73","64","54","49","44","40","34","29","24","20","15","12","11","10","09","08","07"};
-
-  for(int i = 0; i < num.size() ; ++i ) filenames.push_back(dir + num[i] + suffix);
-  
-  redshifts.push_back(0.04603);                       // maximum redshift
-  redshifts.push_back(0.1131);
-  redshifts.push_back(0.1636);
-  redshifts.push_back(0.2279);
-  redshifts.push_back(0.2849);
-  redshifts.push_back(0.3256);
-  redshifts.push_back(0.4156);
-  redshifts.push_back(0.4916);
-  redshifts.push_back(0.5618);
-  redshifts.push_back(0.6383);
-  redshifts.push_back(0.7053);
-  redshifts.push_back(0.7976);
-  redshifts.push_back(0.8868);
-  redshifts.push_back(1.0);
-  redshifts.push_back(1.445);
-  redshifts.push_back(2.145);
-  redshifts.push_back(2.484);
-  redshifts.push_back(2.891);
-  redshifts.push_back(3.0);
-  
-  
-  // shift the redshifts to between the snapshots
-  for(int i=1;i<redshifts.size()-1;++i){
-    redshifts[i] = 0.5*(redshifts[i] + redshifts[i+1]);
+  /*{
+    std::string dir = "/data1/users/gustavo/BigMD/2.5_3840_Planck1/DM_SAMPLES/dm_particles_snap_0";
+    std::string suffix = ".dat";
+    
+    std::vector<std::string> num = {"77","73","64","54","49","44","40","34","29","24","20","15","12","11","10","09","08","07"};
+    
+    for(int i = 0; i < num.size() ; ++i ) snap_filenames.push_back(dir + num[i] + suffix);
+    
+    snap_redshifts.push_back(0.04603);
+    snap_redshifts.push_back(0.1131);
+    snap_redshifts.push_back(0.1636);
+    snap_redshifts.push_back(0.2279);
+    snap_redshifts.push_back(0.2849);
+    snap_redshifts.push_back(0.3256);
+    snap_redshifts.push_back(0.4156);
+    snap_redshifts.push_back(0.4916);
+    snap_redshifts.push_back(0.5618);
+    snap_redshifts.push_back(0.6383);
+    snap_redshifts.push_back(0.7053);
+    snap_redshifts.push_back(0.7976);
+    snap_redshifts.push_back(0.8868);
+    snap_redshifts.push_back(1.0);
+    snap_redshifts.push_back(1.445);
+    snap_redshifts.push_back(2.145);
+    snap_redshifts.push_back(2.484);
+    snap_redshifts.push_back(2.891);
+    snap_redshifts.push_back(3.0);
+  }*/
+  {
+    std::string dir = "Data/dm_particles_snap_0";
+    std::string suffix = ".dat";
+    
+    std::vector<std::string> num = {"77"};
+    
+    for(int i = 0; i < num.size() ; ++i ) snap_filenames.push_back(dir + num[i] + suffix);
+    
+    snap_redshifts.push_back(0.04603);
   }
   
   time_t to,t1;
+  time(&to);
+
+  
+  // random number generator
   Utilities::RandomNumbers_NR ran(12709432);
   
-  
-  // random direction
-
-  const int Ncones = 5;
+  // random direction and observers
   std::vector<Point_3d> observers(Ncones);
   std::vector<Point_3d> directions(Ncones);
-  for(auto &xo : observers){
-    xo[0] = ran()*boxwidth;
-    xo[1] = ran()*boxwidth;
-    xo[2] = ran()*boxwidth;
+  for(auto &x : observers){
+    x[0] = ran()*BoxLength;
+    x[1] = ran()*BoxLength;
+    x[2] = ran()*BoxLength;
   }
   for(auto &v : directions){
     v[0] = ran.gauss();
@@ -93,63 +112,56 @@ int main(int arg,char **argv){
     v /= v.length();
   }
   
-  
-  /**********************************************
-  // *** for tests ***
-  filenames.clear();
-  filenames.push_back("file_example.dat");
-  redshifts.resize(2);
-  redshifts[1] = 2.5;
-  // aim the first 4 cones at the first four halos
-  Point_3d tmp;
-  tmp[0]=3.54674; tmp[1] = 1.12347; tmp[2]=13.69941;
-  tmp /= 0.677700;
-  directions[0] = tmp - observers[0];
-  std::cout << " r = " << directions[0].length() << std::endl;
-  tmp[0]=0.92366; tmp[1] =  9.52555; tmp[2]= 16.51622;
-  tmp /= 0.677700;
-  directions[1] = tmp - observers[1];
-  std::cout << " r = " << directions[1].length() << std::endl;
-  tmp[0]=9.45172; tmp[1] =  3.61210; tmp[2]= 8.19601;
-  tmp /= 0.677700;
-  directions[2] = tmp - observers[2];
-  std::cout << " r = " << directions[2].length() << std::endl;
-  tmp[0]=3.42866; tmp[1] =  2.09676; tmp[2]= 13.78150;
-  tmp /= 0.677700;
-  directions[3] = tmp - observers[3];
-  std::cout << " r = " << directions[3].length() << std::endl;
-  // **********************************************/
-  
-  MultiLightCone hcone(degreesTOradians,observers,directions);
-  //std::vector<std::vector<LightCone::DataRockStar> > conehalos(Ncones);
-  std::vector<std::vector<Point_3d> > cone_particles(Ncones);
-  for(auto &c : cone_particles) c.reserve(100000);
-  
-  time(&to);
-  for(int i=0 ; i < filenames.size() ; ++i){
+  LightCones::FastLightCones(cosmo,zsources,maps,range
+                                  ,angular_resolution
+                                  ,observers
+                                  ,directions
+                                  ,snap_filenames
+                                  ,snap_redshifts
+                                  ,BoxLength
+                                  ,particle_mass
+                                  ,true);
+
+  // output the maps
+  for(int icone = 0 ; icone < Ncones/2 ; ++icone){
+    for(int i=0 ; i < zsources.size() ; ++i){
+      maps[icone][i].printFITS("!" + outdir + "kappa_c" + std::to_string(icone) + "z"
+                               + std::to_string(zsources[i]) + ".fits" );
+    }
+  }
+  // caclulate and output power spectra
+  int N = 50;
+  std::vector<PosType> pspectrum(N),multipole(N);
+  for(int i=0 ; i < zsources.size() ; ++i){
+    std::vector<PosType> totalpower(N,0);
+    for(int icone = 0 ; icone < Ncones ; ++icone){
+      maps[icone][i].PowerSpectrum(pspectrum,multipole);
+      
+      {
+        std::ofstream ps_file(outdir + "kappa_c" + std::to_string(icone) + "z"
+                              + std::to_string(zsources[i]) + "PS" + ".csv");
+        ps_file << "l,PS" << endl;
+        for(int ii=0;ii<pspectrum.size();++ii){
+          ps_file << multipole[ii] << "," << pspectrum[ii] << endl;
+        }
+        ps_file.close();
+      }
+      
+      for(int ii=0 ; ii<N ; ++ii ) totalpower[ii] += pspectrum[ii]/Ncones;
+    }
     
-    std::cout << "Reading from catalog: " << filenames[i] << " z = " << redshifts[i] << " to "
-    << redshifts[i+1] << std::endl;
-    //hcone.ReadBoxRockStar(filenames[i]
-    //                     ,cosmo.coorDist(redshifts[i])
-    //                     ,cosmo.coorDist(redshifts[i+1])
-    //                     ,conehalos);
-    
-    hcone.ReadBoxXYZ(filenames[i]
-                     ,cosmo.coorDist(redshifts[i])
-                     ,cosmo.coorDist(redshifts[i+1])
-                     ,cone_particles
-                     ,hubble,boxwidth);
+    {
+      std::ofstream ps_file(outdir + "kappaAve_z"
+                            + std::to_string(zsources[i]) + "PS" + ".csv");
+      ps_file << "l,PS" << endl;
+      for(int ii=0;ii<N;++ii){
+        ps_file << multipole[ii] << "," << totalpower[ii] << endl;
+      }
+      ps_file.close();
+    }
   }
   
-  //for(int i=0;i<Ncones;++i){
-  //  std::cout << "Number of halos in the cones: " << conehalos[i].size() << std::endl;
-  //  LightCone::WriteLightCone("cone_output_p" + std::to_string(i) + ".csv", conehalos[i]);
-  //}
-  for(int i=0;i<Ncones;++i){
-    std::cout << "Number of halos in the cones: " << cone_particles[i].size() << std::endl;
-    LightCone::WriteLightCone("cone_output_p" + std::to_string(i) + ".csv", cone_particles[i]);
-  }
+  
   time(&t1);
   
   std::cout << "time for constructing cone : " << difftime(t1,to)/60 << " min"
